2af551ee0c5131b23c30867f631eb81e03a4e9a5
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2023-09-16 13:29:26 -0700

    remove `eval` from `bind`

--- src/function/bind.ts
+++ src/function/bind.ts
@@ -1,19 +1,6 @@
-// https://gist.github.com/xulapp/1116996
-export function bind(fn: Function, thisArg: any, ...argArray: any[]) {
-	const method = fn;
-	const args = Array.prototype.slice.call(arguments, 2);
-
-	return function bound() {
-		const _args = args.concat(Array.prototype.slice.call(arguments));
-
-		if (!(fn instanceof bound)) {
-			return method.apply(thisArg, _args);
-		}
-
-		const __args = [];
-		for (var i = 0, len = _args.length; i < len; i++)
-			__args.push("_args[" + i + "]");
-
-		return eval("new method(" + __args.join(",") + ")");
+// https://dev.to/uddeshjain/creating-your-own-bind-polyfill-of-bind-433j
+export function bind(thisArg: any, obj: any, ...args: any[]) {
+	return function (...newArgs: any[]) {
+		thisArg.apply(obj, [...args, ...newArgs]);
 	};
 }
