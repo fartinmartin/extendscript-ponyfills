0eba97a89b7d477952ae44451195e47f8e73a14e
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2026-03-09 18:26:08 -0700

    ya

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'tsdown';
+import { copyStaticAssets } from 'build-utils/copy-assets';
+// import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
 	entry: ['src/index.ts'],
+	// entry: { [`extendscript-ponyfills-v${pkg.version}`]: 'src/index.ts' },
 	format: ['esm'],
+	// outExtensions: () => ({ js: '.jsx' }),
 	clean: true,
 	dts: true,
+	plugins: [
+		{
+			name: 'strip-region-comments',
+			renderChunk(code: string) {
+				return code.replace(/\/\/#(end)?region[^\n]*\n?/g, '');
+			},
+		},
+	],
+	onSuccess: () => {
+		copyStaticAssets('src', 'dist');
+	},
 });
